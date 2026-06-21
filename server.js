dd1d1d1d4928c70beb5d0b03a0dcc7d41bfb96e2
speedyraven <mwikalikathae@gmail.com>
2026-06-21 18:45:43 +0300

    Fix: read Firebase credentials from environment variable

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const cors    = require('cors');
+const admin   = require('firebase-admin');
 
-const app = express();
+const app  = express();
+
+// Use environment variable instead of file
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
